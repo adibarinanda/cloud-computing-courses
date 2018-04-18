18f9a3456b9afaeb5cd1be31e61af402d4e83699
adibarinanda <arinanda.adib@gmail.com>
2018-04-18 08:01:55 +0700

    bootstrap.sh ketinggalan

---
+++ Tugas 2/loadbalancer1/bootstrap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+apt-get install nginx -y
+apt-get install php7.0-fpm -y
---
+++ Tugas 2/worker1/bootstrap.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+apt-get install apache2
+apt-get install libapache2-mod-php7.0 php7.0-fpm -y
